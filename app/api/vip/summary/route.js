ad41ddd8939ada3239b69841d8e2dfafb8904476
zivojinovicmarko10-lab <zivojinovicmarko10@gmail.com>
2026-07-20 17:09:33 +0200

    Add summary API route

---
+++ app/api/vip/summary/route.js
@@ -0,0 +1,5 @@
+import { json, queryFromRequest, repository } from "../repository.js";
+
+export async function GET(request) {
+  return json(await repository.getSummary(queryFromRequest(request)));
+}
